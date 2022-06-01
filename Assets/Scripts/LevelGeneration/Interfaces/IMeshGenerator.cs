86c770a075434f151abe37edf131b49c59aa2f28
Frolov <frolovart161@gmail.com>
2022-06-01 10:58:55 +0700

    Add project files.

---
+++ Assets/Scripts/LevelGeneration/Interfaces/IMeshGenerator.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.LevelGeneration
+{
+    public interface IMeshGenerator
+    {
+        public Mesh GenerateMesh(int xSize, int ySize, Vector3 startedPoint);
+    }
+}
---
+++ Assets/Scripts/LevelGeneration/LevelConstructor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.LevelGeneration
+{
+    public class LevelConstructor : MonoBehaviour
+    {
+        private void Awake()
+        {
+            var meshGenerator = new MeshGenerator();
+
+            var mesh = meshGenerator.GenerateMesh(10, 10, Vector3.zero);
+            GetComponent<MeshFilter>().mesh = mesh;
+            GetComponent<MeshCollider>().sharedMesh = mesh;
+
+            var cloth = gameObject.AddComponent(typeof(Cloth)) as Cloth;
+            cloth.useGravity = false;
+        }
+
+        private void Start()
+        {
+
+        }
+    }
+}
